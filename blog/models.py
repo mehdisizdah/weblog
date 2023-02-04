a600f5544ab3887fd5398c760e99dd12cd4b6490
mehdisizdah <msc13.ict@gmail.com>
2023-02-04 20:22:10 +0330

    add some fields in models and change timezone

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from account.models import User
+from django.utils import timezone
+# from datetime import datetime
 
 # Create your models here.
 
 class Post(models.Model):
     title=models.CharField(max_length=100)
     desctiption=models.TextField()
-    author=models.ForeignKey(User,on_delete=models.PROTECT)
+    author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    created=models.DateTimeField(auto_now_add=True )
+    updated=models.DateTimeField(auto_now=True)
+    publish=models.DateTimeField(default=timezone.now)
