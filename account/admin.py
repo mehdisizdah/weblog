a2774b90ee2844a70cff02dbb1428b770b08f944
mehdisizdah <msc13.ict@gmail.com>
2023-02-28 13:30:40 +0330

    solve admin panel password hash problem

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
+# from django.contrib.admin import ModelAdmin
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
